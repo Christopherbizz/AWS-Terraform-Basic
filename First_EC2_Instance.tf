d28c351f2636bdcbb0643e30b9da4ce83d1afe19
Christopherbizz <96215655+Christopherbizz@users.noreply.github.com>
2024-11-03 22:45:04 -0500

    Update First_EC2_Instance.tf

--- First_EC2_Instance.tf
+++ First_EC2_Instance.tf
@@ -1,10 +1,8 @@
-provider "aws" {
-    region = "us-east-1"
+resource "aws_instance" "test" {
+  ami           = "ami-06b21ccaeff8cd686"  
+  instance_type = "t2.micro"
 
-}
-
-resource "aws_instance" "demo-server" {
-    ami = "ami-04ff98ccbfa41c9ad"
-    key_name = "rtp-03"
-    instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
